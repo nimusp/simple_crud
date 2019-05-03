726b87e4c7cc882210c2d4032db52f1ead19cdaa
Pavel <harrison754@icloud.com>
2019-05-03 22:41:59 +0300

    Добавлен драйвер, модель и утилитный класс для Connection

--- src/main/java/myself/inc/model/User.java
+++ src/main/java/myself/inc/model/User.java
@@ -4,5 +4,61 @@
 
 package myself.inc.model;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class User {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String email;
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/myself/inc/util/DbUtil.java
+++ src/main/java/myself/inc/util/DbUtil.java
@@ -4,5 +4,35 @@
 
 package myself.inc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DbUtil {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            Properties properties = new Properties();
+            InputStream stream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
+            try {
+                properties.load(stream);
+                String driver = properties.getProperty("driver");
+                String url = properties.getProperty("url");
+                String user = properties.getProperty("user");
+                String password = properties.getProperty("password");
+                DriverManager.registerDriver((Driver) Class.forName(driver).newInstance());
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
 }

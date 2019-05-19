72764ddc77da0685129728272b986c1d708dfeca
Pavel <harrison754@icloud.com>
2019-05-20 00:01:12 +0300

    Рефакторинг

--- src/main/java/myself/inc/dao/UserDao.java
+++ src/main/java/myself/inc/dao/UserDao.java
@@ -9,22 +9,20 @@ import myself.inc.util.DbUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
 
-    private final Connection connection;
-
-    public UserDao() {
-        this.connection = DbUtil.getConnection();
-    }
-
     public void addUser(@NotNull User user) {
         String dbRequest = "INSERT INTO users (first_name, last_name, dob, email) VALUES (?, ?, ?, ?)";
         Object[] paramList = new Object[] { user.getFirstName(), user.getLastName(), user.getDateOfBirth(), user.getEmail() };
-        try (PreparedStatement statement = getStatementWithParams(connection, dbRequest, paramList)) {
+        try (Connection dbConnection = DbUtil.getConnection();
+             PreparedStatement statement = getStatementWithParams(dbConnection, dbRequest, paramList)) {
             statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error on add " + user);
@@ -35,7 +33,8 @@ public class UserDao {
     public void updateUser(@NotNull User user) {
         String dbRequest = "UPDATE users SET first_name = ?, last_name = ?, dob = ?, email = ? WHERE user_id = ?";
         Object[] paramList = new Object[] { user.getFirstName(), user.getLastName(), user.getDateOfBirth(), user.getEmail(), user.getUserId()};
-        try (PreparedStatement statement = getStatementWithParams(connection, dbRequest, paramList)) {
+        try (Connection dbConnection = DbUtil.getConnection();
+             PreparedStatement statement = getStatementWithParams(dbConnection, dbRequest, paramList)) {
             statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error on update " + user);
@@ -45,7 +44,8 @@ public class UserDao {
 
     public void deleteUser(@NotNull User user) {
         String dbRequest = "DELETE FROM users WHERE user_id = ?";
-        try (PreparedStatement statement = getStatementWithParams(connection, dbRequest, user.getUserId())) {
+        try (Connection dbConnection = DbUtil.getConnection();
+             PreparedStatement statement = getStatementWithParams(dbConnection, dbRequest, user.getUserId())) {
             statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error on delete " + user);
@@ -56,7 +56,8 @@ public class UserDao {
     @NotNull
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try (ResultSet resultSet = connection.createStatement().executeQuery(
+        try (Connection dbConnection = DbUtil.getConnection();
+             ResultSet resultSet = dbConnection.createStatement().executeQuery(
                 "SELECT * FROM users"
         )) {
             while (resultSet.next()) {
@@ -79,7 +80,8 @@ public class UserDao {
     public User getUserById(int userId) {
         User user = null;
         String dbRequest = "SELECT * FROM users WHERE user_id = ?";
-        try (ResultSet resultSet = getStatementWithParams(connection, dbRequest, userId).executeQuery()) {
+        try (Connection dbConnection = DbUtil.getConnection();
+             ResultSet resultSet = getStatementWithParams(dbConnection, dbRequest, userId).executeQuery()) {
             if (resultSet.next()) {
                 user = new User(
                         resultSet.getInt("user_id"),
--- src/main/java/myself/inc/util/DbUtil.java
+++ src/main/java/myself/inc/util/DbUtil.java
@@ -6,6 +6,7 @@ package myself.inc.util;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -14,10 +15,10 @@ import java.util.Properties;
 
 public class DbUtil {
 
-    private static Connection connection;
+    private static DataSource dataSourceInstance;
 
-    public static Connection getConnection() {
-        if (connection == null) {
+    public static Connection getConnection() throws SQLException {
+        if (dataSourceInstance == null) {
             Properties properties = new Properties();
             InputStream stream = DbUtil.class.getClassLoader().getResourceAsStream("db.properties");
             try {
@@ -29,12 +30,12 @@ public class DbUtil {
                 dataSource.setDatabaseName("my_database");
                 dataSource.setUser(user);
                 dataSource.setPassword(password);
-                connection = dataSource.getConnection();
+                dataSourceInstance = dataSource;
             } catch (IOException | SQLException e) {
                 e.printStackTrace();
             }
         }
 
-        return connection;
+        return dataSourceInstance.getConnection();
     }
 }
